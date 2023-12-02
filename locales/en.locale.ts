f12df0478e6032e09d940583f362aad3e28107c1
separatrixxx <nikitalochmatov@gmail.com>
2023-12-02 11:47:07 +0300

    update

---
+++ locales/en.locale.ts
@@ -0,0 +1,7 @@
+export const en = {
+    locale: "en",
+    language: "English",
+    main_title: "MOEX Hack",
+    error404: "Page not found, 404 error",
+    error500: "Server failure, error 500",
+};
---
+++ locales/ru.locale.ts
@@ -0,0 +1,7 @@
+export const ru = {
+    locale: "ru",
+    language: "Русский",
+    main_title: "MOEX Hack",
+    error404: "Страница не найдена, ошибка 404",
+    error500: "Нарушение работы сервера, ошибка 500",
+};
